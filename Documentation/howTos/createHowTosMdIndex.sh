3bf7eec6616ec01ca29a4953c081854d1cddc5c1
lowlevel86 <cmogged@gmail.com>
2018-08-20 19:09:19 -0500

    update doc

--- Documentation/howTos/createHowTosMdIndex.sh
+++ Documentation/howTos/createHowTosMdIndex.sh
@@ -8,8 +8,8 @@ ls -d ./*/ | sed "s/$/*.md/" | sed "s/^/echo /" | sh | while read -r fileToRead 
    folderName=$(echo "$fileToRead" | sed -n -e 's,^.*/,,p' | sed "s/.md//")
    
    # print html url link for the c file
-   echo "[$folderName.c]($folderName/$folderName.md)" >> index.md
+   echo "[$folderName.c]($folderName/$folderName.md)  " >> index.md
    
-   echo "" >> index.md
+   #echo "" >> index.md
    
 done
